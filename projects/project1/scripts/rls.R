4bf9bab8ca1b79d2ed722e639daa56fcaefb6f3a
Bodurri Klainti <kbontourri@gmail.com>
2017-10-16 18:14:02 +0300

    Forgot to calc phi before plot line

--- projects/project1/scripts/rls.R
+++ projects/project1/scripts/rls.R
@@ -34,6 +34,7 @@ N<-length(X)
 P<-solve(t(x_train_rls)%*%x_train_rls)[1]*diag(n+1) # inverse of A transpose * A, where A=training_set!
 mu<-0.9
 
+
 for (i in 1:N){ # Package 'knitr' must be installed?!
   rls.step<-rls(c(1, X[i]),y[i],t,P,mu)
   t<-rls.step[[1]]
@@ -42,7 +43,12 @@ for (i in 1:N){ # Package 'knitr' must be installed?!
 
 # plot after RLS
 plot(x_train_rls, y_train_rls,main=paste("Forgetting factor mu<-",mu),xlab="x",ylab="y")
-points(X[1:i],y[1:i],col = "blue")
+points(X[1:i],y[1:i],col = "blue") # Push 'escape' to get out of the plot loop
 
-lines(x_train_rls,phi%*%w_rls,col="black")
-lines(X[1:i],cbind(array(1,c(i,1)), X[1:i])%*%t,col="blue")
+# 80% data
+phi = matrix(c(x_train_rls^0,x_train_rls^1), nrow = dimensions-N , ncol = 2) 
+lines(x_train_rls,phi%*%w_rls,col="red")
+
+# after RLS
+lines(X[1:N],cbind(array(1,c(N,1)), X[1:N])%*%t,col="blue")
+
